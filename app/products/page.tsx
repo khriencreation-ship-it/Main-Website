e8073df8256a3005bb9cd5cb08af66d226bb847b
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-01-25 23:43:21 -0800

    update: created the products pages

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,17 +1,16 @@
-import HeroSection from "./HeroSection"
-import PainSection from "./PainSection"
-import FeaturesSection from "./FeaturesSection"
-import AudienceSection from "./AudienceSection"
-import AccessSection from "./AccessSection"
-import TechnologySection from "./TechnologySection"
-import ProductCta from "./ProductCta"
 import Footer from "@/components/Footer"
 
 const page = () => {
     return (
         <>
-        <main className="px-4 lg:px-9 min-h-screen">
-        
+        <main className="px-4 lg:px-9 min-h-screen bg-red-300 mt-[12vh] md:mt-[14vh]">
+           <section className=" mx-10">
+            <div className="flex justify-center items-center">
+                <div className="bg-brandGreen"></div>
+                <div className="bg-brandGreen"></div>
+                <div className="bg-brandGreen"></div>
+            </div>
+            </section> 
         </main>
         <Footer/>
         </>
